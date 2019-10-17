b2daf1155d41a5a2255fa9f7adf471bfaf7d3828
Jen Lindner <jenlindner22@gmail.com>
2019-10-17 22:23:43 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -5,8 +5,8 @@ while row_index < src.count do
   while element_index < src[row_index].count do
   "I love #{[src][row_index][element_index][0]} and #{[src][row_index
       element_index += 1
+    end
       row_index += 1
- end
 end
 end
   
