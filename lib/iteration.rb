f9b5b23682b01d44263ecab93528f69ebe1b6469
Jen Lindner <jenlindner22@gmail.com>
2019-10-17 22:30:05 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -11,9 +11,7 @@ end
 end
   
   # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
-  #
-  # Build a new Array that contains strings where each pair of foods is
-  # inserted into this template:
+  # Build a new Array that contains strings where each pair of foods is inserted into this template:
   # "I love (inner array element 0) and (inner array element 1) on my pizza""
   # As such, there should be a new String for each inner array, or pair
 
