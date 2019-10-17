b0de2545195e310aafc069df686a459c13163388
Jen Lindner <jenlindner22@gmail.com>
2019-10-17 22:21:44 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -2,6 +2,7 @@ def join_ingredients(src)
   row_index = 0
 while row_index < src.count do
   element_index = 0
+  while element_index < src[row_index].count do
  puts "I love #{[src][row_index][element_index][0]} and #{[src][row_index][element_index][1]} on my pizza"
  element_index += 1
  end
